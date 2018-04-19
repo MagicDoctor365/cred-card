fd483428b3b0b41ca1491f369889515aa8135b6a
James Zhang <675041082@qq.com>
2018-04-19 23:24:01 +0800

    add member-point

--- pages/card-market/card-market.js
+++ pages/card-market/card-market.js
@@ -2,6 +2,10 @@ var mydata = require("./data.js");
 
 Page({
   data: {
-    cardlist: mydata.cardlist
+    cardlist: mydata.cardlist,
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   }
 })
---
+++ pages/shopping-mall/data.js
@@ -0,0 +1,10 @@
+var bannerImgUrl = [
+  "https://img1.soonwill.com/SWPat/adv/2017/11/1_171106104947.jpg",
+  "https://img1.soonwill.com/SWPat/adv/2018/3/1_180322153314.jpg",
+  "https://img1.soonwill.com/SWPat/adv/2018/3/1_180309094617.jpg",
+  "https://img.soonwill.com/adv//2017/1/1_170113093100.jpg"
+]
+
+module.exports = {
+  bannerImgUrl: bannerImgUrl
+}
